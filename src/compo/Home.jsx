3c37f361d73e888c42bbef138495cfef672f2ae2
Ankit Chemjong <npp.rasik@gmail.com>
2024-06-28 21:22:26 +0545

    hello

--- src/compo/Home.jsx
+++ src/compo/Home.jsx
@@ -1,15 +1,18 @@
+import { useContext } from 'react';
 import Card from './Card.jsx';
-// import {AppCreate} from './Create.jsx';
-
+import {AppContext} from './Create.jsx';
 export default function Home() {
-  // const {phone,name}=useContext(AppCreate);
+  const {phone,name}=useContext(AppContext);
   return (
   <>
   <div className='row flex-row justify-content-evenly mt-5'>
   <Card src='rn.jpg' title="cris" desc="Hello cris"/>
   <Card src='cons.jpg' title="cons of business" desc="Hello cons"/>
   </div>
-
+   <div className='row'>
+     <p>{phone}</p>
+     <p>{name}</p>
+   </div>
   </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import ContextProvider from './compo/Create.jsx';
+import ContextProvider from './compo/Create.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(  
+    <ContextProvider>
         <App />
+    </ContextProvider>
 )
